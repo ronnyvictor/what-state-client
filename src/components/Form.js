96ceb1b5a62f9dd1e459d2e40d5e3b44c2428d86
Roneil Victor <roneilvictor@Ronnys-MacBook-Pro.local>
2022-03-02 16:28:22 -0500

    form has onchange function

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,15 +1,29 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 
 export default function Form() {
 	const [form, setForm] = useState('')
+
+	const handleSubmit = (event) => {
+		event.preventDefault()
+	}
+
+	const onChange = (event) => {
+		setForm(event.target.value)
+	}
+
 	return (
-		<>
+		<section>
 			<h1>What State Is This?</h1>
-			<form>
-				<input type='text' />
+			<form onSubmit={handleSubmit}>
+				<input
+					type='text'
+					placeholder='Press enter to submit'
+					onChange={onChange}
+					value={form}
+				/>
 				<br />
 				<a href='/'>Skip</a>
 			</form>
-		</>
+		</section>
 	)
 }
--- src/components/StateMap.js
+++
@@ -1,9 +0,0 @@
-import usaMap from '../usa-map.svg'
-
-export default function StateMap () {
-  return (
-    <>
-    <img src={usaMap} alt="Map of the United States of America" />
-    </>
-  )
-}
